07eba0b30c221063df6aa4109599de3d2332c93f
bintangmadaanilazuardy <bintangmadanilazuardy@gmail.com>
2021-01-22 08:47:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calculator/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_activity);
+        //untuk menghilangkan action Bar
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
